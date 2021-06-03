550eda444b92ce139985dd05f9018045466f9e1f
Usman Farooq <anovius10@gmail.com>
2021-06-03 17:27:35 +0500

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express()
+const router = express.Router()
+const nodemailer = require('nodemailer');
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use('/', router)
+
+app.post('/sendEmail', (req, res) => {
+    const {receiver} = req.body
+    const {subject} = req.body
+    const {message} = req.body
+
+    var messageToSend = {
+        from: "Your Email Here",
+        to: receiver, 
+        subject:  subject,
+        text: message,
+        html: ""
+    }
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'Your Email Here',
+            pass: 'Your Password Here*'
+        }
+    })
+
+    transporter.sendMail(messageToSend, (err, info) => {
+        if(err) 
+            res.status(421).send({message: 'Something went wrong check out data and send again'})
+        else
+            res.status(200).send({message: 'Email sent successfully'})
+    })
+})
+
+app.listen(3000)
